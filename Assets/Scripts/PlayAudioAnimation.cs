467e20b9dc4b9b5ce0ba5adcb552dfea320ec820
leandersonralves <leo_ribeiro_alves@yahoo.com.br>
2020-09-26 18:25:23 -0300

    Util para tocar som a partir de msg.

---
+++ Assets/Scripts/PlayAudioAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Classe utilitaria com funcao para tocar som originado de SendMessage ou Animation Event.
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class PlayAudioAnimation : MonoBehaviour
+{
+    /// <summary>
+    /// Instancia para AudioSource que a funcao toca o AudioClip.
+    /// </summary>
+    [SerializeField]
+    private AudioSource audioSource;
+
+    /// <summary>
+    /// Lista de AudioClip que podem ser tocados.
+    /// </summary>
+    /// <typeparam name="AudioClip">AudioClip do Unity.</typeparam>
+    public List<AudioClip> audioClips = new List<AudioClip>();
+
+    public void PlayAudioClip(string audioClipName)
+    {
+        var audioClip = audioClips.Find(x => x.name.Equals(audioClipName));
+        if (audioClip)
+        {
+            audioSource.PlayOneShot(audioClip);
+        }
+        else
+        {
+            Debug.LogWarning("AudioClip (" + audioClipName + ") nao localizado no AudioSource (" + name + ")");
+        }
+    }
+}
